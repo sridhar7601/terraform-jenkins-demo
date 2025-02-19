c085985723a3ce2f406cd1879c5092bc6200f71d
Sridhar <sridzar1234@gmail.com>
2025-02-19 11:20:17 +0530

    Refactor variable names in terraform/variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,1 @@
 # terraform/outputs.tf
-
-variable "env" {
-  description = "The environment to deploy to (dev, stage, prod)"
-  type        = string
-}
-
-variable "bucket_name_prefix" {
-  default     = "myapp-bucket"
-  description = "Prefix for S3 bucket name"
-}
