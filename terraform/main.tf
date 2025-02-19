ee6e205d05b9c48fced29d1fc77725cbe9d78947
Sridhar <sridzar1234@gmail.com>
2025-02-19 11:22:08 +0530

    Refactor variable and S3 bucket names in terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,17 +2,6 @@
 provider "aws" {
   region = "us-east-1"
 }
-
-variable "env" {
-  description = "The environment to deploy to (dev, stage, prod)"
-  type        = string
-}
-
-variable "bucket_name_prefix" {
-  default     = "myapp-bucket"
-  description = "Prefix for S3 bucket name"
-}
-
 resource "aws_s3_bucket" "demo_bucket" {
   bucket = "${var.bucket_name_prefix}-${var.env}"
   
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,10 @@
 # terraform/outputs.tf
+variable "env" {
+  description = "Deployment environment"
+  type        = string
+}
+
+variable "bucket_name_prefix" {
+  description = "S3 bucket name prefix"
+  type        = string
+}
