5eb31bc5bf13991fee9cfe5e87ef6dc631cfbdbd
Sridhar <sridzar1234@gmail.com>
2025-02-19 08:59:51 +0530

    Refactor S3 bucket name in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "demo_bucket" {
-  bucket = "jenkins-terraform-sru-${var.environment}"
+  bucket = "jenkins-terraform-srsu-${var.environment}"
   force_destroy = true
 }
 
