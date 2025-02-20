f3328ef21b82231e27e1e90446041d6919a6f7f0
Sridhar <sridzar1234@gmail.com>
2025-02-20 08:44:51 +0530

    Refactor AWS region in Terraform configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,10 @@
 # backend.tf - Place this in your terraform directory
 terraform {
   backend "s3" {
-    # Empty - values will be supplied by Jenkins pipeline
+    bucket         = "terraform-state-storage-bucket"
+    key            = "terraform/jenkins-demo/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table" # Optional but recommended for state locking
+    encrypt        = true
   }
 }
